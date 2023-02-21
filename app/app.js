4c0a6e8980025ea684a2ae908ce3ecd2e1a69137
LewPearce <lew@DESKTOP-0NCRVJR>
2023-02-21 16:39:15 +0000

    add review id endpoint and error handling

--- app/app.js
+++ app/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const { getCategories } = require("./controllers/categories_controllers");
-const { getReviews } = require("./controllers/reviews_constrollers");
-const { handle404 } = require("./controllers/error-controllers");
+const {
+  getReviews,
+  getReviewByID,
+} = require("./controllers/reviews_constrollers");
+const {
+  handle404,
+  handle400,
+  handleCustomErrors,
+} = require("./controllers/error-controllers");
 
 const app = express();
 
@@ -9,6 +16,10 @@ app.get("/api/categories", getCategories);
 
 app.get("/api/reviews", getReviews);
 
+app.get("/api/reviews/:review_id", getReviewByID);
+
+app.use(handle400);
 app.use(handle404);
+app.use(handleCustomErrors);
 
 module.exports = app;
--- app/models/reviews_models.js
+++ app/models/reviews_models.js
@@ -23,4 +23,24 @@ const fetchReviews = () => {
     });
 };
 
-module.exports = { fetchReviews };
+const fetchReviewByID = (req) => {
+  let { params } = req;
+  return db
+    .query(
+      `SELECT * FROM reviews
+  WHERE review_id = $1`,
+      [params.review_id]
+    )
+    .then(({ rows }) => {
+      console.log(rows);
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `Oops! ID:${params.review_id} doesn't exist!`,
+        });
+      } else {
+        return rows[0];
+      }
+    });
+};
+module.exports = { fetchReviews, fetchReviewByID };
