101c5ffec71716a3a9ea1d89a806f322c239c8e6
LewPearce <lew@DESKTOP-0NCRVJR>
2023-02-24 16:45:17 +0000

    added queries (only functional as indivisula queries not concurrent)

--- app/controllers/reviews_constrollers.js
+++ app/controllers/reviews_constrollers.js
@@ -7,8 +7,11 @@ const {
 } = require("../models/reviews_models");
 
 const getReviews = (req, res, next) => {
-  fetchReviews()
-    .then((result) => res.status(200).send({ reviews: result }))
+  let { query } = req;
+  fetchReviews(query)
+    .then((result) => {
+      res.status(200).send({ reviews: result });
+    })
     .catch((err) => {
       next(err);
     });
